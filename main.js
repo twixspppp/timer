d7db34e2a5c45fa64bde84f2c83dbc241b16bf6f
Odina <toshtemirova07@gmail.com>
2024-12-16 10:49:57 +0300

    timer push

---
+++ main.js
@@ -0,0 +1,8 @@
+let hrs = document.getElementById("hrs")
+let min = document.getElementById("min")
+let sec = document.getElementById("sec")
+
+let currentTime = new Date()
+
+
+console.log(currentTime);
